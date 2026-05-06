df85a7b11576c611b81d82ff5db04b54f73c11cd
Mr Krot <vkorotanec@gmail.com>
2026-05-06 17:15:24 +0200

    Memory leaks fixed and multiple template symbols support

--- cwc.c
+++ cwc.c
@@ -3,6 +3,7 @@
 // `cwc` -> same as `cwc site build-site`
 // `cwc some_site` -> also valid, `some_site` is compiled into `build-site`
 
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,15 +16,16 @@
 #include <stdbool.h>
 
 #define TEMPLATE_FILE "template.html"
-#define TEMPLATE_SYMBOL "%"
-#define TEMPLATE_SYMBOL_CHAR '%'
+// these are just rarely used together in html
+#define TEMPLATE_SYMBOL "@%"
+#define TEMPLATE_SYMBOL_LENGTH (sizeof(TEMPLATE_SYMBOL)-1)
 
 #define DEFAULT_INPUT_DIR "site/"
 #define DEFAULT_OUTPUT_DIR "build-site/"
 
 #define RELATIVE_PATH_PREFIX "./"
 
-#define IGNORE_DIRECTORY RELATIVE_PATH_PREFIX DEFAULT_INPUT_DIR "assets/"
+#define IGNORE_DIRECTORY "assets/"
 
 char* err_str;
 
@@ -46,11 +48,11 @@ char* err_str;
     
 #define HANDLE_ERROR(e) HANDLE_ERROR_VALUE(e, -1)
 
-#define UNBALANCED_TEMPLATE_SYMBOL_ERROR				 \
-  do {	 								 \
-    fprintf(stderr, "Error: unbalanced template symbol at %s\n", first); \
-    exit(-1);								 \
-  } while (0)								 \
+#define UNBALANCED_TEMPLATE_SYMBOL_ERROR(file_str)			\
+  do {									\
+    fprintf(stderr, "Error: unbalanced template symbol at %s\n", file_str); \
+    exit(-1);								\
+  } while (0)								\
 
 // strdup cuz we may modify it later
 void setIoDirs(int argc, char** argv, char** input_dir, char** output_dir) {
@@ -70,6 +72,11 @@ void setIoDirs(int argc, char** argv, char** input_dir, char** output_dir) {
 }
 
 typedef struct {
+  char* data;
+  size_t size;
+} OwningStringView;
+
+typedef struct {
   const char* data;
   size_t size;
 } StringView;
@@ -78,12 +85,12 @@ typedef struct {
   size_t length;
   size_t capacity;
   StringView* tokens;
-  const char* original_string; // this is \0 terminated
+  char* original_string; // this is \0 terminated
 } TemplateFile;
 
-StringView readFile(const char *filename) {
+OwningStringView readFile(const char *filename) {
   FILE *fp = fopen(filename, "rb"); // rb just in case, it does nothing on linux
-  if(!fp) return (StringView){.data = NULL, .size = 0};
+  if(!fp) return (OwningStringView){.data = NULL, .size = 0};
 
   fseek(fp, 0, SEEK_END);
   int fsize = ftell(fp);
@@ -93,10 +100,10 @@ StringView readFile(const char *filename) {
   fread(fcontent, 1, fsize, fp);
   fclose(fp);
 
-  return (StringView){ .data = fcontent, .size = fsize};
+  return (OwningStringView){ .data = fcontent, .size = fsize};
 }
 
-const char* readFileC(const char* filename) {
+char* readFileC(const char* filename) {
   FILE *fp = fopen(filename, "rb"); // rb just in case, it does nothing on linux
   if(!fp) return NULL;
 
@@ -112,6 +119,10 @@ const char* readFileC(const char* filename) {
   return fcontent;
 }
 
+void freeOwingStringView(OwningStringView* str) {
+  free(str->data);
+}
+
 // i'm sorry it's 2 AM i don't know what to call these
 typedef struct {
   StringView token; // %body% for example
@@ -122,31 +133,37 @@ typedef struct {
   size_t length;
   size_t capacity;
   RegularFileToken* tokens;
-  const char* original_text;
+  char* original_text;
 } RegularFile;
 
 RegularFile serializeFile(const char* file_path) {
   RegularFile result = {0};
+  result.original_text = readFileC(file_path);
   result.capacity = 4;
   result.tokens = malloc(result.capacity * sizeof(RegularFileToken));
-  result.original_text = readFileC(file_path);
 
-  const char* text_to_search = result.original_text;
+  char* text_to_search = result.original_text;
   
   while (1) {
-    const char* first = strstr(text_to_search, TEMPLATE_SYMBOL);
+    char* first = strstr(text_to_search, TEMPLATE_SYMBOL);
     if (first == NULL) break;
-    const char* second = strstr(first+1, TEMPLATE_SYMBOL);
-    if (second == NULL) UNBALANCED_TEMPLATE_SYMBOL_ERROR;
+    if (first != result.original_text) {
+      if (*(first-1) == '\\') {
+	text_to_search = first + TEMPLATE_SYMBOL_LENGTH; // to skip the template symbol
+	continue;
+      }
+    }
+    char* second = strstr(first+TEMPLATE_SYMBOL_LENGTH, TEMPLATE_SYMBOL);
+    if (second == NULL) UNBALANCED_TEMPLATE_SYMBOL_ERROR(file_path);
 
     size_t length = second - first;
 
     size_t till_next_template_symbol = 0;
-    const char* next_template_symbol = strstr(second+1, TEMPLATE_SYMBOL);
+    char* next_template_symbol = strstr(second + TEMPLATE_SYMBOL_LENGTH, TEMPLATE_SYMBOL);
     if (next_template_symbol == NULL)
-      till_next_template_symbol = strlen(second+1); // means we reached the end
+      till_next_template_symbol = strlen(second + TEMPLATE_SYMBOL_LENGTH); // means we reached the end
     else
-      till_next_template_symbol = next_template_symbol - (second + 1);
+      till_next_template_symbol = next_template_symbol - (second + TEMPLATE_SYMBOL_LENGTH);
 
     if (result.capacity <= result.length+1) {
       result.capacity += 4;
@@ -154,21 +171,26 @@ RegularFile serializeFile(const char* file_path) {
     }
     result.tokens[result.length] = (RegularFileToken) {
       .token = (StringView) {.data = first, .size = length},
-      .body = (StringView) {.data = second+1, .size = till_next_template_symbol}
+      .body = (StringView) {.data = second+TEMPLATE_SYMBOL_LENGTH, .size = till_next_template_symbol}
     };
       
     result.length++;
 
-    text_to_search = second+1;
+    text_to_search = second+TEMPLATE_SYMBOL_LENGTH;
   }
 
   return result;
 }
 
+void freeRegularFile(RegularFile* rf) {
+  free(rf->original_text);
+  free(rf->tokens);
+}
+
 // the processed file is at `output_file_path`
 // kinda n(O^2)
 void processOneFile(TemplateFile template, const char* file_path, const char* output_file_path) {
-  printf("[FILE] Processing %s file to %s.\n", file_path, output_file_path);
+  printf("[FILE] Processing %s file and outputing to %s.\n", file_path, output_file_path);
   RegularFile input_file = serializeFile(file_path);
   FILE* output_file = fopen(output_file_path, "w");
 
@@ -181,15 +203,20 @@ void processOneFile(TemplateFile template, const char* file_path, const char* ou
     size_t token_size = 0;
     // then we write the corresponding token from the file
     for (size_t j = 0; j < input_file.length; j++) {
-      if (strncmp(template.tokens[i].data, input_file.tokens[j].token.data, template.tokens[0].size) == 0) {
+      if (strncmp(template.tokens[i].data, input_file.tokens[j].token.data, template.tokens[i].size) == 0) {
 	fwrite(input_file.tokens[j].body.data, 1, input_file.tokens[j].body.size, output_file);
 	token_size = input_file.tokens[j].token.size;
       }
     }
+    // means we couldn't find a token
+    if (token_size == 0) {
+      fprintf(stderr, "[  WARNING  ] No matching tag %.*s in the file %s.\n", (int)(template.tokens[i].size+TEMPLATE_SYMBOL_LENGTH), template.tokens[i].data, file_path);
+    }
 
-    left_at_string += till_next_token + token_size + 1;
+    left_at_string += till_next_token + token_size + TEMPLATE_SYMBOL_LENGTH;
   }
   fwrite(left_at_string, 1, strlen(left_at_string), output_file);
+  freeRegularFile(&input_file);
 }
 
 void ensureDirExists(const char* path) {
@@ -200,18 +227,21 @@ void ensureDirExists(const char* path) {
 }
 
 void copyFile(const char* path, const char* output_path) {
-  StringView input_file = readFile(path);
+  OwningStringView input_file = readFile(path);
   FILE* output_file = fopen(output_path, "wb");
+
   fwrite(input_file.data, 1, input_file.size, output_file);
+  
+  freeOwingStringView(&input_file);
   fclose(output_file);
 }
 
-void processDirInternal(const char* path, const char* output_path, TemplateFile template, bool just_copy) {
+void processDirInternal(const char* path, const char* output_path, TemplateFile template, bool just_copy, const char* ignore_directory) {
   printf("[ DIR] Processing %s dir and outputing to %s.\n", path, output_path);
 
   ensureDirExists(output_path);
 
-  just_copy = just_copy ? just_copy : strcmp(path, IGNORE_DIRECTORY) == 0;
+  just_copy = just_copy ? just_copy : strcmp(path, ignore_directory) == 0;
   
   DIR* input_dir = opendir(path);
   HANDLE_ERROR_VALUE(input_dir, NULL);
@@ -230,7 +260,7 @@ void processDirInternal(const char* path, const char* output_path, TemplateFile 
     stat(current_file_path, &stats);
     switch(stats.st_mode & S_IFMT) {
     case S_IFREG: { // if it's a file
-      if (strcmp(current_file->d_name, TEMPLATE_FILE) == 0) continue;
+      if (strcmp(current_file->d_name, TEMPLATE_FILE) == 0) goto loopEnd;
       char* output_file_path = NULL;
       PATH_CONC(&output_file_path, output_path, current_file->d_name);
 
@@ -250,11 +280,11 @@ void processDirInternal(const char* path, const char* output_path, TemplateFile 
       output_dir_path[len+1] = '\0';
 
       size_t len2 = strlen(current_file_path);
-      current_file_path = realloc(current_file_path, len2);
+      current_file_path = realloc(current_file_path, len2 + 2);
       current_file_path[len2] = '/';
       current_file_path[len2+1] = '\0';
       
-      processDirInternal(current_file_path, output_dir_path, template, just_copy);
+      processDirInternal(current_file_path, output_dir_path, template, just_copy, ignore_directory);
       free(output_dir_path);
       break;
     }
@@ -263,14 +293,15 @@ void processDirInternal(const char* path, const char* output_path, TemplateFile 
       assert(false);
       break;
     }
+  loopEnd:
     free(current_file_path);
   }
   
   closedir(input_dir);
 }
 
-void processDir(const char* path, const char* output_path, TemplateFile template) {
-  processDirInternal(path, output_path, template, false);
+void processDir(const char* path, const char* output_path, TemplateFile template, const char* ignore_directory) {
+  processDirInternal(path, output_path, template, false, ignore_directory);
 }
 
 char* makeActualPath(char* input) {
@@ -302,8 +333,14 @@ TemplateFile processTemplate(const char* template_path) {
   while (1) {
     char* first = strstr(string, TEMPLATE_SYMBOL);
     if (first == NULL) break;
-    char* second = strstr(first+1, TEMPLATE_SYMBOL);
-    if (second == NULL) UNBALANCED_TEMPLATE_SYMBOL_ERROR;
+    if (first != result.original_string) {
+      if (*(first-1) == '\\') {
+	string = first + TEMPLATE_SYMBOL_LENGTH;
+	continue;
+      }
+    }
+    char* second = strstr(first+TEMPLATE_SYMBOL_LENGTH, TEMPLATE_SYMBOL);
+    if (second == NULL) UNBALANCED_TEMPLATE_SYMBOL_ERROR(template_path);
 
     size_t length = second - first;
     if (result.capacity <= result.length+1) {
@@ -312,10 +349,15 @@ TemplateFile processTemplate(const char* template_path) {
     }
     result.tokens[result.length] = (StringView){.data = first, .size = length};
     result.length++;
-    string = second+1;
+    string = second + TEMPLATE_SYMBOL_LENGTH;
   }
 
   return result;
+}
+
+void freeTemplate(TemplateFile* tf) {
+  free(tf->original_string);
+  free(tf->tokens);
 }
 
 int main(int argc, char** argv) {
@@ -331,12 +373,17 @@ int main(int argc, char** argv) {
   PATH_CONC(&template_file_path, input_dir_str, TEMPLATE_FILE);
   TemplateFile template = processTemplate(template_file_path);
   free(template_file_path);
-  
-  processDir(input_dir_str, output_dir_str, template);
 
+  char* ignore_directory = NULL;
+  PATH_CONC(&ignore_directory, input_dir_str, IGNORE_DIRECTORY);
+  
+  processDir(input_dir_str, output_dir_str, template, ignore_directory);
+
+  freeTemplate(&template);
   free(err_str);
   free(input_dir_str);
   free(output_dir_str);
   free(base_input_dir_str);
   free(base_output_dir_str);
+  free(ignore_directory);
 }
